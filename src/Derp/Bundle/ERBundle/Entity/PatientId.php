43227af517455498b296db4a43f24340f445586a
Matthias Noback <matthiasnoback@gmail.com>
2015-09-12 21:01:35 +0200

    Collapse hierarchy of entity ids

--- src/Derp/Bundle/ERBundle/Entity/PatientId.php
+++ src/Derp/Bundle/ERBundle/Entity/PatientId.php
@@ -2,6 +2,24 @@
 
 namespace Derp\Bundle\ERBundle\Entity;
 
-class PatientId extends EntityId
+class PatientId
 {
+    private $id;
+
+    private function __construct()
+    {
+    }
+
+    public static function fromString($string)
+    {
+        $patientId = new static();
+        $patientId->id = $string;
+
+        return $patientId;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->id;
+    }
 }
